152e513e168dc6e52486df16c600a1f9529b8cfe
kylinxm <kylinxm@qq.com>
2015-11-14 11:51:10 +0800

    修正使用前摄像头推流问题

--- sdk/RTMPKit.framework/Headers/RTMPPublisher.h
+++ sdk/RTMPKit.framework/Headers/RTMPPublisher.h
@@ -27,6 +27,13 @@ typedef NS_ENUM(NSInteger, RTMPOrientation) {
     RTMPOrientation_LandscapeLeft      = 4,
 };
 
+typedef NS_ENUM(NSInteger, RTMPCameraPosition) {
+    RTMPCameraPosition_Unspecified         = 0,
+    RTMPCameraPosition_Back                = 1,
+    RTMPCameraPosition_Front               = 2
+   
+};
+
 @protocol RTMPPublisherDelegate <NSObject>
 
 @required
@@ -41,11 +48,11 @@ typedef NS_ENUM(NSInteger, RTMPOrientation) {
 -(void) onStopped:(RTMPPublisher*) sender;
 @end
 
+
+
+
 @interface RTMPPublisher : RTMPStream
-{
-    @package AudioParam* _audioParam;
-    @package VideoParam* _videoRequestParam;
-}
+
 
 @property (nonatomic) UIView* view;
 
@@ -60,6 +67,23 @@ typedef NS_ENUM(NSInteger, RTMPOrientation) {
 -(void) setAudioParam:(int) sampleRateInHZ bitrate:(int) bitrate;
 -(void) setVideoParam:(int) width height:(int) height frameRate:(int) frameRate bitRate:(int) bitRate;
 -(void) setVideoOrientation:(RTMPOrientation) orientation;
+
+/**
+ * 设定前后摄像头
+ *
+ * @position 
+ *
+ */
+-(void) setCameraPosition:(RTMPCameraPosition) position;
+
+/**
+ * @autoOrientation
+ * 
+ * 设定视频输出的视频方向是否自动按视图的方向进行转换
+ * 默认值为 false， 设定为 true 时 setVideoOrientation 设定值回被自动改变
+ *
+ */
+@property BOOL autoOrientation;
 
 -(void)switchCam;
 -(void) startPreview;
